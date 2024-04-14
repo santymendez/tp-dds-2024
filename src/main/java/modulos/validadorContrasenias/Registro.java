0b3d70f668f3c28268478ddc1748bf9eed8cfbe0
santymendez <santiagomendez532@gmail.com>
2024-04-14 19:16:53 -0300

    quitamos los ToDo de la clase registro
    Co-authored-by: liamwilk <lwilk@frba.utn.edu.ar>
    Co-authored-by: ilorences <ilorences@frba.utn.edu.ar>
    Co-authored-by: MatiasJastrebow <mjastrebow@frba.utn.edu.ar>
    Co-authored-by: AAMazzini03 <amazzini@frba.utn.edu.ar>

--- src/main/java/modulos/validadorContrasenias/Registro.java
+++ src/main/java/modulos/validadorContrasenias/Registro.java
@@ -2,8 +2,6 @@ package modulos.validadorContrasenias;
 
 import lombok.Getter;
 
-import java.util.HashSet;
-
 public class Registro {
     private Usuario usuario;
     @Getter
@@ -14,9 +12,6 @@ public class Registro {
     }
 
     public Boolean sePuedeRegistrar(Usuario usuario){
-        //TODO
-        // posiblemente habria que hace algo con el nombre de usuario
-        // no creo que se pueda poner cualquiera
         return this.controlador.esValida(usuario.getContrasenia());
     }
 }

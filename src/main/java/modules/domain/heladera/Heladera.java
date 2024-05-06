b2c345af4170008475f9a7cc3ab42cbdc06b21fc
liamwilk <liamewilk@gmail.com>
2024-05-06 13:56:02 -0300

    fix: arreglos para checkstyle y el build no ejecuta los tests

--- src/main/java/modules/domain/heladera/Heladera.java
+++ src/main/java/modules/domain/heladera/Heladera.java
@@ -1,6 +1,5 @@
 package modules.domain.heladera;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import lombok.Getter;
@@ -21,31 +20,32 @@ public class Heladera {
   private Integer capacidadMaximaViandas;
   private List<Vianda> viandas;
   private Date fechaDeCreacion;
+  private Modelo modelo;
+  private SensorMovimiento sensorMovimiento;
 
   /**
    * Se inicializa la heladera.
    *
-   * @param direccion es la direccion actual de la heladera.
-   * @param nombre es el nombre de la heladera.
+   * @param direccion              es la direccion actual de la heladera.
+   * @param nombre                 es el nombre de la heladera.
    * @param capacidadMaximaViandas es la capacidad maxima de viandas.
-   * @param sensorMovimiento es el sensor de movimiento propio de la heladera.
-   * @param modelo es el modelo de la heladera.
-   * @param fechaDeCreacion fecha en la que se coloco la heladera.
-   * @param viandas es una lista con las viandas que hay dentro de la heladera.
+   * @param sensorMovimiento       es el sensor de movimiento propio de la heladera.
+   * @param modelo                 es el modelo de la heladera.
+   * @param fechaDeCreacion        fecha en la que se coloco la heladera.
+   * @param viandas                es una lista con las viandas que hay dentro de la heladera.
    */
-  public Heladera(Direccion direccion, String nombre, Integer capacidadMaximaViandas,
-                  SensorMovimiento sensorMovimiento, Modelo modelo, Date fechaDeCreacion, List<Vianda> viandas) {
+
+  public Heladera(Direccion direccion, String nombre,
+                  Integer capacidadMaximaViandas, List<Vianda> viandas, Date fechaDeCreacion,
+                  Modelo modelo, SensorMovimiento sensorMovimiento) {
     this.direccion = direccion;
     this.nombre = nombre;
     this.capacidadMaximaViandas = capacidadMaximaViandas;
-    this.sensorMovimiento = sensorMovimiento;
-    this.modelo = modelo;
+    this.viandas = viandas;
     this.fechaDeCreacion = fechaDeCreacion;
-    this.viandas = new ArrayList<>();
+    this.modelo = modelo;
+    this.sensorMovimiento = sensorMovimiento;
   }
-
-  private Modelo modelo;
-  private SensorMovimiento sensorMovimiento;
 
   public void agregarVianda(Vianda vianda) {
     this.viandas.add(vianda);

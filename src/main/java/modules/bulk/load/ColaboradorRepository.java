49af521f906b875cab9bef05cfe80a4794f2462d
liamwilk <liamewilk@gmail.com>
2024-05-21 18:09:07 -0300

    fix: arreglos para checkstyle

--- src/main/java/modules/bulk/load/ColaboradorRepository.java
+++ src/main/java/modules/bulk/load/ColaboradorRepository.java
@@ -9,18 +9,21 @@ import modules.domain.colaboracion.Colaboracion;
 import modules.domain.personas.colaborador.Colaborador;
 import modules.email.sender.EmailSender;
 
+/**
+ * Clase de Colaborador para cargar los datos del CSV.
+ */
 @Getter
 @Setter
 public class ColaboradorRepository {
   //TODO hacer logica
-    private String tipoDocumento;
-    private String numeroDocumento;
-    private String nombre;
-    private String apellido;
-    private String email;
-    private String fecha;
-    private String tipoColaboracion;
-    private Integer cantidad;
+  private String tipoDocumento;
+  private String numeroDocumento;
+  private String nombre;
+  private String apellido;
+  private String email;
+  private String fecha;
+  private String tipoColaboracion;
+  private Integer cantidad;
 
 
-  }
+}

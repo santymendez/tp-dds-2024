2f0f6ddddcae0829ef254d89001e30733c17319a
AAMazzini03 <amazzini@frba.utn.edu.ar>
2024-10-13 21:23:16 -0300

    Tu esqueleto te trajo hasta aquí con un cuerpo hambriento, veloz y aquí ¡Gracias a dios! uno no cree en lo que oye. Checkstyle de la soledad y de la desolación preso de tu ilusión vas a fallar, a fallar... fallar.

--- src/main/java/utils/medical/entities/Persona.java
+++ src/main/java/utils/medical/entities/Persona.java
@@ -11,6 +11,5 @@ import lombok.Getter;
 public class Persona {
   //Necesitamos que se llame asi para poder integrarnos
   //Usamos el suppress para evitar problemas con el checkstyle
-  @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
   private String nombreYApellido;
 }

8800312b9d2e014bf7d8d745353efcad8e3e63e4
liamwilk <liamewilk@gmail.com>
2024-11-03 16:00:55 -0300

    feat: comienzo metricas
    Co-authored-by: ilorences <ilorences@frba.utn.edu.ar>
    Co-authored-by: AAMazzini03 <amazzini@frba.utn.edu.ar>

---
+++ src/main/java/metrics/ApprovalException.java
@@ -0,0 +1,12 @@
+package metrics;
+
+import java.io.Serial;
+
+/**
+ * Approval exception.
+ */
+
+public class ApprovalException extends Exception {
+  @Serial
+  private static final long serialVersionUID = 1;
+}

9d6bed66f945ba930a4f6aac17d35a9fc5c4a504
liamwilk <liamewilk@gmail.com>
2024-11-14 15:45:49 -0300

    feat: uso de crons

--- src/main/java/server/App.java
+++ src/main/java/server/App.java
@@ -1,86 +1,76 @@
 package server;
 
-//import cronjobs.CalculoHacerseCargo;
-//import cronjobs.DetectorFallaDesconexion;
-//import cronjobs.GeneradorReporteCronJob;
-//import org.quartz.CronScheduleBuilder;
-//import org.quartz.JobBuilder;
-//import org.quartz.JobDetail;
-//import org.quartz.Scheduler;
-//import org.quartz.SchedulerException;
-//import org.quartz.SchedulerFactory;
-//import org.quartz.SimpleScheduleBuilder;
-//import org.quartz.Trigger;
-//import org.quartz.TriggerBuilder;
-//import org.quartz.impl.StdSchedulerFactory;
+import cronjobs.CalculoHacerseCargo;
+import cronjobs.DetectorFallaDesconexion;
+import cronjobs.GeneradorReporteCronJob;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
 
 /**
  * Clase principal de la aplicación del servidor.
  */
-
 public class App {
-
-  /**
-   * Main de la aplicación del servidor.
-   *
-   * @param args Argumentos de la línea de comandos.
-   */
 
   public static void main(String[] args) {
     Server.init();
-    //this.startScheduler();
+    startScheduler();
   }
 
-  // private static void startScheduler() {
-  //   try {
-  //     SchedulerFactory sf = new StdSchedulerFactory();
-  //     Scheduler scheduler = sf.getScheduler();
-  //
-  //     JobDetail jobCalculoHacerseCargo = JobBuilder.newJob(CalculoHacerseCargo.class)
-  //         .withIdentity("jobCalculoHacerseCargo", "colaboraciones")
-  //         .usingJobData("Info", "Valor")
-  //         .build();
-  //
-  //     Trigger triggerCalculoHacerseCargo = TriggerBuilder.newTrigger()
-  //         .withIdentity("triggerCalculoHacerseCargo", "colaboraciones")
-  //         .startNow()
-  //         .withSchedule(CronScheduleBuilder.monthlyOnDayAndHourAndMinute(1, 0, 0))
-  //         .build();
-  //
-  //     JobDetail jobReportes = JobBuilder.newJob(GeneradorReporteCronJob.class)
-  //         .withIdentity("jobReportes", "reportes")
-  //         .usingJobData("Info", "Valor")
-  //         .build();
-  //
-  //     Trigger triggerReportes = TriggerBuilder.newTrigger()
-  //         .withIdentity("triggerReportes", "reportes")
-  //         .startNow()
-  //         .withSchedule(CronScheduleBuilder.weeklyOnDayAndHourAndMinute(1, 0, 0))
-  //         .build();
-  //
-  //     JobDetail jobDetectorFallaConexion = JobBuilder.newJob(DetectorFallaDesconexion.class)
-  //         .withIdentity("jobDetectorFallaConexion", "sensores")
-  //         .usingJobData("Info", "Valor")
-  //         .build();
-  //
-  //     Trigger triggerDetectorFallaConexion = TriggerBuilder.newTrigger()
-  //         .withIdentity("triggerDetectorFallaConexion", "sensores")
-  //         .startNow()
-  //         .withSchedule(
-  //             SimpleScheduleBuilder.simpleSchedule()
-  //                 .withIntervalInMinutes(5)
-  //                 .repeatForever())
-  //         .build();
-  //
-  //     scheduler.scheduleJob(jobCalculoHacerseCargo, triggerCalculoHacerseCargo);
-  //     scheduler.scheduleJob(jobReportes, triggerReportes);
-  //     scheduler.scheduleJob(jobDetectorFallaConexion, triggerDetectorFallaConexion);
-  //
-  //     scheduler.start();
-  //
-  //   } catch (SchedulerException e) {
-  //     e.printStackTrace();
-  //   }
-  // }
+  private static void startScheduler() {
+    try {
+      SchedulerFactory sf = new StdSchedulerFactory();
+      Scheduler scheduler = sf.getScheduler();
 
+      JobDetail jobCalculoHacerseCargo = JobBuilder.newJob(CalculoHacerseCargo.class)
+          .withIdentity("jobCalculoHacerseCargo", "colaboraciones")
+          .usingJobData("Info", "Valor")
+          .build();
+
+      Trigger triggerCalculoHacerseCargo = TriggerBuilder.newTrigger()
+          .withIdentity("triggerCalculoHacerseCargo", "colaboraciones")
+          .startNow()
+          .withSchedule(CronScheduleBuilder.monthlyOnDayAndHourAndMinute(1, 0, 0))
+          .build();
+
+      JobDetail jobReportes = JobBuilder.newJob(GeneradorReporteCronJob.class)
+          .withIdentity("jobReportes", "reportes")
+          .usingJobData("Info", "Valor")
+          .build();
+
+      Trigger triggerReportes = TriggerBuilder.newTrigger()
+          .withIdentity("triggerReportes", "reportes")
+          .startNow()
+          .withSchedule(CronScheduleBuilder.weeklyOnDayAndHourAndMinute(1, 0, 0))
+          .build();
+
+      // JobDetail jobDetectorFallaConexion = JobBuilder.newJob(DetectorFallaDesconexion.class)
+      //     .withIdentity("jobDetectorFallaConexion", "sensores")
+      //     .usingJobData("Info", "Valor")
+      //     .build();
+      //
+      // Trigger triggerDetectorFallaConexion = TriggerBuilder.newTrigger()
+      //     .withIdentity("triggerDetectorFallaConexion", "sensores")
+      //     .startNow()
+      //     .withSchedule(
+      //         SimpleScheduleBuilder.simpleSchedule()
+      //             .withIntervalInMinutes(5)
+      //             .repeatForever())
+      //     .build();
+      //
+      // scheduler.scheduleJob(jobCalculoHacerseCargo, triggerCalculoHacerseCargo);
+      // scheduler.scheduleJob(jobReportes, triggerReportes);
+      // scheduler.scheduleJob(jobDetectorFallaConexion, triggerDetectorFallaConexion);
+      scheduler.start();
+
+    } catch (SchedulerException e) {
+      e.printStackTrace();
+    }
+  }
 }
